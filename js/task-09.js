ab9a8fe503340d82126fecb57de4f1a03cff654a
Anna <anjanadvorna@gmail.com>
2023-09-04 14:12:25 +0300

    Update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = { 
+  button: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+  body: document.querySelector('body'),
+} 
+
+refs.button.addEventListener('click', onColorChange); 
+
+function onColorChange() {
+
+const bodyColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = bodyColor;
+  refs.color.textContent = bodyColor;
+}
